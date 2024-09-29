cb5fd2197fdc0f8dc3e9da4a16c50ac53eed2dc7
김용민 <dydals3440@gmail.com>
2024-09-29 17:18:08 +0900

    feat: Client Component에서의 ServerAction 구현

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,8 +1,9 @@
 'use server';
 
-import { revalidatePath } from 'next/cache';
+import { revalidatePath, revalidateTag } from 'next/cache';
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(state: any, formData: FormData) {
+  console.log(state, '상태');
   // 이 값이 있을 때에만 문자열로 확정
   // 기본 form-data 타입이 FormDataEntryValue | null 이기 떄문.
   const content = formData.get('content')?.toString();
@@ -12,8 +13,11 @@ export async function createReviewAction(formData: FormData) {
   const bookId = formData.get('bookId')?.toString();
 
   // 예외처리 (값 X -> 서버액션 종료)
-  if (!content || !author) {
-    return;
+  if (!content || !author || !bookId) {
+    return {
+      status: false,
+      error: '리뷰 내용과 작성자를 입력해주세요.',
+    };
   }
 
   try {
@@ -30,10 +34,21 @@ export async function createReviewAction(formData: FormData) {
       },
     );
 
-    // 5. 태그 값을 기준으로 재검증
-    revalidatePath(`review-${bookId}`);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // 5. 태그 값을 기준으로 재검증s
+    revalidateTag(`review-${bookId}`);
+
+    return {
+      status: true,
+      error: '',
+    };
   } catch (error) {
-    console.error(error);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${error}`,
+    };
   }
 }
